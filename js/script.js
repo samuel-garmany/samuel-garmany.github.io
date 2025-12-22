e74f5bba97ec0e72ac16a397964594b1e7b96704
Sam Garmany <65299214+samuel-garmany@users.noreply.github.com>
2025-12-22 06:36:39 +0000

    couple tiny fixes/improvements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 fetch('https://api.github.com/repos/samuel-garmany/samuel-garmany.github.io/commits?per_page=1')
     .then(r => r.json())
-    .then(d => document.getElementById('last-updated').textContent = new Date(d[0].commit.committer.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }));
+    .then(d => document.getElementById('last-updated').textContent = new Date(d[0].commit.committer.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }))
+    .catch(() => document.getElementById('last-updated').textContent = 'Unknown');
